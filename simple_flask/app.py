12389f01b35bdff73b5c86516006d8a1433439a7
axel36 <khramov@alexey.by>
2020-06-21 01:22:22 +0300

    add simple app

--- simple_flask/app.py
+++ simple_flask/app.py
@@ -1,1 +1,10 @@
-# new flask app
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello, stranger, walk your road"
+
+if __name__ == '__main__':
+    app.run()
